8f7094e38ca2f660d6a61ad2586f9dd3f0334541
whj <whj@whjdeMacBook-Pro.local>
2016-03-07 22:26:56 +0800

    Initial Commit

---
+++ 0307-新浪微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0307-新浪微博
+//
+//  Created by whj on 16/3/7.
+//  Copyright © 2016年 wanghongjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0307-新浪微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0307-新浪微博
+//
+//  Created by whj on 16/3/7.
+//  Copyright © 2016年 wanghongjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
